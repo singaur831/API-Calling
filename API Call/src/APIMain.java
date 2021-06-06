a7dbc2b1f185ca81ca8ea3e4c83ef5f44f11d414
singaur831 <gaurav13@github.com>
2021-06-06 16:00:33 +0530

    Java Api

---
+++ API Call/src/APIMain.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class APIMain {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        var url = "https://github.com/singaur831";
+        //httprequest//
+        var request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+        var client = HttpClient.newBuilder().build();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+    }
+
+}
